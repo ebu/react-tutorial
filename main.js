5fe64666efde657e697b10149731fe8e03ab4cf1
barroco <barroco@ebu.ch>
2015-09-05 16:20:41 +0200

    Change listening port to 8000

--- main.js
+++ main.js
@@ -38,16 +38,6 @@ var express = require('express'),
 //  res.json(tenLines);
 //});
 
-app.get('/status', function (req, res) {
-  res.json({"status": "ok"})
-});
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 /**
  * Migrants API
@@ -92,11 +82,25 @@ rd.on('pause', function() {
   console.log("Data ready.", data.length);
 })
 
+
 app.get('/data/summary', function (req, res) {
   res.json(db);
 });
 
-var server = app.listen(80, function () {
+
+app.get('/status', function (req, res) {
+  res.json({"status": "ok"})
+});
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
