b73b2b9b79b4994860e8f4d908a0ff4b344bede0
barroco <barroco@ebu.ch>
2015-09-05 15:49:53 +0200

    WIP Comment GeoIP api

--- main.js
+++ main.js
@@ -5,38 +5,38 @@ var express = require('express'),
   CSV = require('csv-string'),
   filename = process.argv[2],
   lines = [];
-
-
-var rd = readline.createInterface({
-    input: fs.createReadStream(filename, {encoding: 'utf-8'}),
-    output: process.stdout,
-    terminal: false
-});
-
-rd.on('line', function(line) {
-    var splittedLine = line.split("\t");
-    var newLineJSON = {
-        'ip': splittedLine[0],
-        'city': splittedLine[1],
-        'country': splittedLine[2],
-        'latitude': splittedLine[3],
-        'longitude': splittedLine[4],
-        'date': splittedLine[5],
-        'request': splittedLine[6].replace(/"/g, ""),
-        'status_code': splittedLine[7],
-        'content_size': splittedLine[8]
-    };
-
-    lines.push(newLineJSON);
-});
-
-app.get('/data', function (req, res) {
-  var tenLines =[];
-  for(var i = 0; i < 10; i++) {
-      tenLines.push(lines[Math.floor(Math.random()*lines.length)]);
-  }
-  res.json(tenLines);
-});
+//
+//
+//var rd = readline.createInterface({
+//    input: fs.createReadStream(filename, {encoding: 'utf-8'}),
+//    output: process.stdout,
+//    terminal: false
+//});
+//
+//rd.on('line', function(line) {
+//    var splittedLine = line.split("\t");
+//    var newLineJSON = {
+//        'ip': splittedLine[0],
+//        'city': splittedLine[1],
+//        'country': splittedLine[2],
+//        'latitude': splittedLine[3],
+//        'longitude': splittedLine[4],
+//        'date': splittedLine[5],
+//        'request': splittedLine[6].replace(/"/g, ""),
+//        'status_code': splittedLine[7],
+//        'content_size': splittedLine[8]
+//    };
+//
+//    lines.push(newLineJSON);
+//});
+//
+//app.get('/data', function (req, res) {
+//  var tenLines =[];
+//  for(var i = 0; i < 10; i++) {
+//      tenLines.push(lines[Math.floor(Math.random()*lines.length)]);
+//  }
+//  res.json(tenLines);
+//});
 
 app.get('/status', function (req, res) {
   res.json({"status": "ok"})
